3e3ba9ab7a9e5e46b4ff2a1b6e10b46e97bd300a
tarunn@brickred.com <tarunn@brickred.com@3dfb73c5-3865-12c0-3a85-70851a3b1c07>
2010-12-29 18:01:38 +0000

    Added LinkedIn provider

--- trunk/SocialAuth/src/org/brickred/socialauth/AuthProviderFactory.java
+++ trunk/SocialAuth/src/org/brickred/socialauth/AuthProviderFactory.java
@@ -35,6 +35,7 @@ import org.brickred.socialauth.provider.AolImpl;
 import org.brickred.socialauth.provider.FacebookImpl;
 import org.brickred.socialauth.provider.GoogleImpl;
 import org.brickred.socialauth.provider.HotmailImpl;
+import org.brickred.socialauth.provider.LinkedInImpl;
 import org.brickred.socialauth.provider.OpenIdImpl;
 import org.brickred.socialauth.provider.TwitterImpl;
 import org.brickred.socialauth.provider.YahooImpl;
@@ -56,6 +57,7 @@ public class AuthProviderFactory {
 	public static String yahoo = "yahoo";
 	public static String hotmail = "hotmail";
 	public static String aol = "aol";
+	public static String linkedin = "linkedin";
 	private static String propFileName = "oauth_consumer.properties";
 	/**
 	 * 
@@ -69,8 +71,7 @@ public class AuthProviderFactory {
 	 */
 	public static AuthProvider getInstance(final String id)
 	throws Exception {
-		AuthProvider provider = getProvider(id, propFileName,
-				AuthProvider.ALL_PERMISSIONS);
+		AuthProvider provider = getProvider(id, propFileName);
 		return provider;
 
 	}
@@ -88,56 +89,13 @@ public class AuthProviderFactory {
 	 */
 	public static AuthProvider getInstance(final String id,
 			final String propertiesFileName) throws Exception {
-		AuthProvider provider = getProvider(id, propertiesFileName,
-				AuthProvider.ALL_PERMISSIONS);
-		return provider;
-
-	}
-
-	/**
-	 * 
-	 * @param id
-	 *            the id of requested provider. It can be google, yahoo,
-	 *            hotmail, twitter, facebook.
-	 * @param scope
-	 *            scope is a permission setting. It can be
-	 *            AuthProvider.AUTHENTICATION_ONLY or
-	 *            AuthProvider.ALL_PERMISSIONS
-	 * @return AuthProvider the instance of requested provider based on given
-	 *         id. If id is a URL it returns the OpenId provider.
-	 * @throws Exception
-	 */
-	public static AuthProvider getInstance(final String id, final int scope)
-	throws Exception {
-		AuthProvider provider = getProvider(id, propFileName, scope);
-		return provider;
-
-	}
-
-	/**
-	 * 
-	 * @param id
-	 *            the id of requested provider. It can be google, yahoo,
-	 *            hotmail, twitter, facebook.
-	 * @param scope
-	 *            scope is a permission setting. It can be
-	 *            AuthProvider.AUTHENTICATION_ONLY or
-	 *            AuthProvider.ALL_PERMISSIONS
-	 * @param propertiesFileName
-	 *            file name to read the properties
-	 * @return AuthProvider the instance of requested provider based on given
-	 *         id. If id is a URL it returns the OpenId provider.
-	 * @throws Exception
-	 */
-	public static AuthProvider getInstance(final String id,
-			final String propertiesFileName, final int scope) throws Exception {
-		AuthProvider provider = getProvider(id, propertiesFileName, scope);
+		AuthProvider provider = getProvider(id, propertiesFileName);
 		return provider;
 
 	}
 
 	private static AuthProvider getProvider(final String id,
-			final String fileName, final int scope) throws Exception {
+			final String fileName) throws Exception {
 		Properties props = new Properties();
 		AuthProvider provider;
 		try {
@@ -146,17 +104,19 @@ public class AuthProviderFactory {
 			props.load(in);
 			props.setProperty("id", id);
 			if (facebook.equals(id)) {
-				provider = new FacebookImpl(props, scope);
+				provider = new FacebookImpl(props);
 			} else if (twitter.equals(id)) {
-				provider = new TwitterImpl(props, scope);
+				provider = new TwitterImpl(props);
 			} else if (aol.equals(id)) {
 				provider = new AolImpl(props);
 			} else if (google.equals(id)) {
-				provider = new GoogleImpl(props, scope);
+				provider = new GoogleImpl(props);
 			} else if (yahoo.equals(id)) {
-				provider = new YahooImpl(props, scope);
+				provider = new YahooImpl(props);
 			} else if (hotmail.equals(id)) {
-				provider = new HotmailImpl(props, scope);
+				provider = new HotmailImpl(props);
+			} else if (linkedin.equals(id)) {
+				provider = new LinkedInImpl(props);
 			} else {
 				provider = new OpenIdImpl(props);
 			}
@@ -164,8 +124,8 @@ public class AuthProviderFactory {
 			throw new FileNotFoundException(fileName
 					+ " file is not found in your class path");
 		} catch (IOException ie) {
-			throw new IOException(
-			"Problem in loading the properties from oauth_consumer.properties file");
+			throw new IOException("Could not load configuration from "
+					+ fileName);
 		} catch (SocialAuthConfigurationException se) {
 			throw se;
 		}
