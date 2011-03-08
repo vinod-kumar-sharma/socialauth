1f0aba00aeb9c767695b1bcc2753fcea2ce0a062
tarunn@brickred.com <tarunn@brickred.com@3dfb73c5-3865-12c0-3a85-70851a3b1c07>
2011-03-08 17:44:35 +0000

    It contains various method those are required for OAUTH

--- trunk/SocialAuth/src/org/brickred/socialauth/util/OAuthConsumer.java
+++ trunk/SocialAuth/src/org/brickred/socialauth/util/OAuthConsumer.java
@@ -24,214 +24,496 @@
  */
 package org.brickred.socialauth.util;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.brickred.socialauth.exception.SignatureException;
+import org.brickred.socialauth.exception.SocialAuthConfigurationException;
 import org.brickred.socialauth.exception.SocialAuthException;
 
-public class OAuthConsumer {
+/**
+ * It contains various method those are required for OAUTH
+ * 
+ * @author tarunn@brickred.com
+ * 
+ */
+public class OAuthConsumer implements Serializable, Constants {
 
-	private String consumerKey;
-	private String consumerSecret;
-	private String accessToken;
-	private String tokenSecret;
-	public static String HMACSHA1_SIGNATURE = "HMAC-SHA1";
-	public OAuthConsumer() {
-	}
-	public OAuthConsumer(String consumerKey, String consumerSecret,
-			String accessToken, String tokenSecret) {
-		this.consumerKey = consumerKey;
-		this.consumerSecret = consumerSecret;
-		this.accessToken = accessToken;
-		this.tokenSecret = tokenSecret;
-	}
+	private static final long serialVersionUID = -4560115102581632124L;
+	private static final Pattern AMPERSAND = Pattern.compile("&");
+	private final Log LOG = LogFactory.getLog(OAuthConsumer.class);
+	private OAuthConfig config;
 
 	/**
-	 * Encode the string for OAuth
 	 * 
-	 * @param value
-	 * @return
-	 * @throws Exception
+	 * @param config
+	 *            Configuration object which contains information of application
+	 *            configuration
 	 */
-	public String encode(String value) throws Exception {
-		if (value == null) {
-			return "";
-		}
-
-		try {
-			return URLEncoder.encode(value, "utf-8")
-			// OAuth encodes some characters differently:
-			.replace("+", "%20").replace("*", "%2A")
-			.replace("%7E", "~");
-			// This could be done faster with more hand-crafted code.
-		} catch (UnsupportedEncodingException wow) {
-			throw new SocialAuthException(wow.getMessage(), wow);
-		}
+	public OAuthConsumer(final OAuthConfig config) {
+		this.config = config;
 	}
 
 	/**
-	 * It returns a signature to make OAuth request.
+	 * It returns a signature for signing OAuth request.
 	 * 
 	 * @param signatureType
 	 *            Type of signature. It can be HMAC-SHA1.
 	 * @param method
 	 *            Method type can be GET, POST or PUT
 	 * @param url
+	 *            Requested URL for which generating signature
 	 * @param args
-	 * @return String the signature
+	 *            Required arguments to generate signature
+	 * @param token
+	 *            Token Object
+	 * @return The computed signature
 	 * @throws Exception
 	 */
-	public String generateSignature(String signatureType, String method,
-			String url, Map<String, String> args) throws Exception {
+	public String generateSignature(final String signatureType,
+			final String method, final String url,
+			final Map<String, String> args, final Token token) throws Exception {
 		if (HMACSHA1_SIGNATURE.equals(signatureType)) {
-			return getHMACSHA1(method, url, args);
+			return getHMACSHA1(method, url, args, token);
 		} else {
-			throw new SocialAuthException(
-			"Signature type is null or not a valid signature type");
+			throw new SignatureException("Signature type not implemented :"
+					+ signatureType);
 		}
 	}
 
-	private String getHMACSHA1(String method, String url,
-			Map<String, String> args) throws Exception {
+	private String getHMACSHA1(final String method, final String url,
+			final Map<String, String> args, final Token token) throws Exception {
 
-		if (consumerSecret == null || consumerSecret.length() == 0) {
-			throw new SocialAuthException("Consumer secret is null");
+		if (config.get_consumerSecret().length() == 0) {
+			throw new SignatureException("Please check consumer secret");
 		}
-		if (tokenSecret == null || tokenSecret.length() == 0) {
-			throw new SocialAuthException("Token secret is null");
+		boolean valid = MethodType.GET.equals(method)
+				|| MethodType.PUT.equals(method)
+				|| MethodType.POST.equals(method);
+		if (!valid) {
+			throw new SignatureException("Invalid method type :" + method);
 		}
-		if (method == null || method.length() == 0) {
-			throw new SocialAuthException("method is null");
+		if (url.length() == 0) {
+			throw new SignatureException("Please check URL");
 		}
-		if (url == null || url.length() == 0) {
-			throw new SocialAuthException("URL is null");
+		String key = HttpUtil.encodeURIComponent(config.get_consumerSecret())
+				+ "&";
+		if (token != null && token.getSecret() != null) {
+			key += token.getSecret();
 		}
-		String key = consumerSecret + "&" + tokenSecret;
 		try {
 			// get an hmac_sha1 key from the raw key bytes
 			SecretKeySpec signingKey = new SecretKeySpec(key.getBytes("UTF-8"),
-			"HMAC-SHA1");
+					"HMAC-SHA1");
 
 			// get an hmac_sha1 Mac instance and initialize with the signing key
 			Mac mac = Mac.getInstance("HmacSHA1");
 			mac.init(signingKey);
 
-			String data = encode(method) + "&" + encode(url) + "&"
-			+ encode(buildParams(args));
+			String data = HttpUtil.encodeURIComponent(method) + "&"
+					+ HttpUtil.encodeURIComponent(url) + "&"
+					+ HttpUtil.encodeURIComponent(HttpUtil.buildParams(args));
 			// compute the hmac on input data bytes
 			byte[] rawHmac = mac.doFinal(data.getBytes("UTF-8"));
 
 			// base64-encode the hmac
-			return Base64.encodeBytes(rawHmac);
+			LOG.debug("Encoding raw HMAC to Base64");
+			String sig = Base64.encodeBytes(rawHmac);
+
+			return sig;
 		} catch (Exception e) {
-			throw new SocialAuthException("Unable to generate HMAC-SHA1", e);
+			throw new SignatureException("Unable to generate HMAC-SHA1", e);
 		}
 	}
 
 	/**
+	 * It obtains the request token
 	 * 
-	 * @param args
-	 * @return String
+	 * @param reqTokenURL
+	 *            Request Token URL
+	 * @param callbackURL
+	 *            Callback URL
+	 * @return The request token
 	 * @throws Exception
 	 */
-	public String buildParams(Map<String, String> args) throws Exception {
-		List<String> argList = new ArrayList<String>();
-		for (String key : args.keySet()) {
-			String arg = key + "=" + encode(args.get(key));
-			argList.add(arg);
+	public Token getRequestToken(final String reqTokenURL,
+			final String callbackURL) throws Exception {
+		boolean valid = reqTokenURL.length() > 0 && callbackURL.length() > 0;
+		Token token = null;
+		Map<String, String> params = new HashMap<String, String>();
+		params.put(OAUTH_CALLBACK, callbackURL);
+		putOauthParams(params);
+		String reqURL = reqTokenURL;
+		String sig = generateSignature(config.get_signatureMethod(),
+				config.get_transportName(), reqURL, params, null);
+		LOG.debug(config.get_signatureMethod() + "Signature : " + sig);
+		params.put(OAUTH_SIGNATURE, sig);
+		reqURL += "?" + HttpUtil.buildParams(params);
+		LOG.debug("URL to get Request Token : " + reqURL);
+
+		Response response = HttpUtil.doHttpRequest(reqURL,
+				config.get_transportName(), null, null);
+
+		if (response.getStatus() == 200) {
+			token = new Token();
+			parse(response.getInputStream(), token);
+		} else {
+			LOG.debug("Error while fetching Request Token");
+			throw new SocialAuthConfigurationException(
+					"Application keys are not correct. "
+							+ "The server running the application should be same that was registered to get the keys.");
 		}
-		Collections.sort(argList);
-		StringBuilder s = new StringBuilder();
-		for (int i = 0; i < argList.size(); i++) {
-			s.append(argList.get(i));
-			if (i != argList.size() - 1) {
-				s.append("&");
-			}
+		return token;
+	}
+
+	/**
+	 * It obtains the access token
+	 * 
+	 * @param accessTokenURL
+	 *            Access Token URL
+	 * @param reqToken
+	 *            Request Token
+	 * @return The Access Token
+	 * @throws Exception
+	 */
+	public Token getAccessToken(final String accessTokenURL,
+			final Token reqToken) throws Exception {
+		if (accessTokenURL == null) {
+			throw new SocialAuthException("Access Token URL is null");
+		}
+		if (reqToken == null) {
+			throw new SocialAuthException("Request Token is null");
+		}
+		if (reqToken.getKey() == null || reqToken.getKey().length() == 0) {
+			throw new SocialAuthException(
+					"Key in Request Token is null or blank");
+		}
+		Map<String, String> params = new HashMap<String, String>();
+		Token accessToken = null;
+		if (reqToken.getAttribute(OAUTH_VERIFIER) != null) {
+			params.put(OAUTH_VERIFIER, reqToken.getAttribute(OAUTH_VERIFIER)
+					.toString());
+		}
+		params.put(OAUTH_TOKEN, reqToken.getKey());
+		putOauthParams(params);
+
+		String reqURL = accessTokenURL;
+		String sig = generateSignature(config.get_signatureMethod(),
+				config.get_transportName(), reqURL, params, reqToken);
+		params.put(OAUTH_SIGNATURE, sig);
+		reqURL += "?" + HttpUtil.buildParams(params);
+		LOG.debug("Access Token URL : " + reqURL);
+		Response response = null;
+		try {
+			response = HttpUtil.doHttpRequest(reqURL,
+					config.get_transportName(), null, null);
+		} catch (Exception e) {
+			LOG.debug("Error while getting Access Token");
+			throw new SocialAuthException("Error while getting Access Token", e);
 		}
 
-		return s.toString();
-	}
-
-	/**
-	 * Retrieves the Consumer Key
-	 * 
-	 * @return String the consumer key
-	 */
-	public String getConsumerKey() {
-		return consumerKey;
-	}
-
-	/**
-	 * Updates the consumer key
-	 * 
-	 * @param consumerKey
-	 *            the consumer key
-	 */
-	public void setConsumerKey(String consumerKey) {
-		this.consumerKey = consumerKey;
-	}
-
-	/**
-	 * Retrieves the consumer secret
-	 * 
-	 * @return String the consumer secret
-	 */
-	public String getConsumerSecret() {
-		return consumerSecret;
-	}
-
-	/**
-	 * Updats the consumer secret
-	 * 
-	 * @param consumerSecret
-	 *            the consumer secret.
-	 */
-	public void setConsumerSecret(String consumerSecret) {
-		this.consumerSecret = consumerSecret;
-	}
-
-	/**
-	 * Retrieves the access token
-	 * 
-	 * @return String the Access Token
-	 */
-	public String getAccessToken() {
+		if (response.getStatus() == 200) {
+			accessToken = new Token();
+			parse(response.getInputStream(), accessToken);
+		} else {
+			throw new SocialAuthException(
+					"Unable to retrieve the access token. Status: "
+							+ response.getStatus());
+		}
 		return accessToken;
 	}
 
-	/**
-	 * Updates the access token
-	 * 
-	 * @param accessToken
-	 *            the access token
-	 */
-	public void setAccessToken(String accessToken) {
-		this.accessToken = accessToken;
+	private void putOauthParams(final Map<String, String> params) {
+		params.put(OAUTH_CONSUMER_KEY, config.get_consumerKey());
+		params.put(OAUTH_SIGNATURE_METHOD, config.get_signatureMethod());
+		params.put(OAUTH_VERSION, CURRENT_VERSION);
+		putNonceAndTimestamp(params);
 	}
 
 	/**
-	 * Retrieves the token secret
+	 * Does an HTTP GET request.
 	 * 
-	 * @return String the token secret
+	 * @param reqURL
+	 *            URL to send request to.
+	 * @param headerParams
+	 *            Header Parameters
+	 * @param token
+	 *            Token to pass in GET request
+	 * @return Response object
+	 * @throws Exception
 	 */
-	public String getTokenSecret() {
-		return tokenSecret;
+	public Response httpGet(final String reqURL,
+			final Map<String, String> headerParams, final Token token)
+			throws Exception {
+		return send(reqURL, null, headerParams, null,
+				MethodType.GET.toString(), token, true);
+	}
+
+	public Response httpGet(final String reqURL,
+			final Map<String, String> params,
+			final Map<String, String> headerParams, final Token token,
+			final boolean isHeaderRequired) throws Exception {
+		return send(reqURL, params, headerParams, null,
+				MethodType.GET.toString(), token, isHeaderRequired);
 	}
 
 	/**
-	 * Updates the token secret
+	 * Does an HTTP POST request.
 	 * 
-	 * @param tokenSecret
-	 *            the token secret
+	 * @param reqURL
+	 *            URL to send request to.
+	 * @param params
+	 *            Any additional parameters whose signature we want to compute.
+	 * @param headerParams
+	 *            Header Parameters
+	 * @param body
+	 *            Request Body
+	 * @param token
+	 *            Token to pass in POST request
+	 * @return Response object
+	 * @throws Exception
 	 */
-	public void setTokenSecret(String tokenSecret) {
-		this.tokenSecret = tokenSecret;
+	public Response httpPost(final String reqURL,
+			final Map<String, String> params,
+			final Map<String, String> headerParams, final String body,
+			final Token token) throws Exception {
+		return send(reqURL, params, headerParams, body,
+				MethodType.POST.toString(), token, true);
+	}
+
+	/**
+	 * 
+	 * @param reqURL
+	 *            URL to send request to.
+	 * @param params
+	 *            Any additional parameters whose signature we want to compute
+	 * @param headerParams
+	 *            Header Parameters
+	 * @param body
+	 *            Request Body
+	 * @param token
+	 *            Token to pass in PUT request
+	 * @return Response object
+	 * @throws Exception
+	 */
+	public Response httpPut(final String reqURL,
+			final Map<String, String> params,
+			final Map<String, String> headerParams, final String body,
+			final Token token) throws Exception {
+		return send(reqURL, params, headerParams, body,
+				MethodType.PUT.toString(), token, true);
+	}
+
+	/**
+	 * @param reqURL
+	 *            URL to send request to.
+	 * @param params
+	 *            Any additional parameters whose signature we want to compute
+	 * @param headerParams
+	 *            Header Parameters
+	 * @param body
+	 *            Request Body
+	 * @param token
+	 *            Token to pass in PUT request
+	 * @param isHeaderRequired
+	 *            True if header is required
+	 * @return Response object
+	 * @throws Exception
+	 */
+	public Response httpPut(final String reqURL,
+			final Map<String, String> params,
+			final Map<String, String> headerParams, final String body,
+			final Token token, final boolean isHeaderRequired) throws Exception {
+		return send(reqURL, params, headerParams, body,
+				MethodType.PUT.toString(), token, isHeaderRequired);
+	}
+
+	private Response send(final String reqURL,
+			final Map<String, String> paramsMap,
+			final Map<String, String> headerParams, final String body,
+			final String methodName, final Token token,
+			final boolean isHeaderRequired) throws Exception {
+		Map<String, String> params;
+		if (paramsMap != null) {
+			params = paramsMap;
+		} else {
+			params = new HashMap<String, String>();
+		}
+		params.put(OAUTH_TOKEN, token.getKey());
+		putOauthParams(params);
+		String url;
+		int idx = reqURL.indexOf('?');
+		if (idx != -1) {
+			String[] pairs = AMPERSAND.split(reqURL.substring(idx + 1));
+			for (String pair : pairs) {
+				int eq = pair.indexOf('=');
+				if (eq == -1) {
+					params.put(pair, "");
+				} else {
+					params.put(pair.substring(0, eq),
+							HttpUtil.decodeURIComponent(pair.substring(eq + 1)));
+				}
+			}
+			url = reqURL.substring(0, idx);
+		} else {
+			url = reqURL;
+		}
+		String sig = generateSignature(config.get_signatureMethod(),
+				methodName, url, params, token);
+		params.put(OAUTH_SIGNATURE, sig);
+		Map<String, String> headerMap = null;
+		if (isHeaderRequired) {
+			String headerVal = getAuthHeaderValue(params);
+			headerMap = new HashMap<String, String>();
+			headerMap.put("Authorization", headerVal);
+			if (headerParams != null) {
+				for (String key : headerParams.keySet()) {
+					headerMap.put(key, headerParams.get(key));
+				}
+			}
+			url = reqURL;
+		} else {
+			url += "?" + HttpUtil.buildParams(params);
+		}
+		return HttpUtil.doHttpRequest(url, methodName, body, headerMap);
+	}
+
+	private void parse(final InputStream in, final Token token)
+			throws Exception {
+		StringBuffer sb = new StringBuffer();
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					in, ENCODING));
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line);
+			}
+		} catch (Exception exc) {
+			throw new SocialAuthException("Failed to parse response");
+		}
+
+		String[] pairs = sb.toString().split("&");
+		String key = null, secret = null;
+		for (String pair : pairs) {
+			int idx = pair.indexOf('=');
+			if (idx == -1) {
+				continue;
+			}
+			String k = pair.substring(0, idx);
+			String v = HttpUtil.decodeURIComponent(pair.substring(idx + 1));
+			if (key == null && OAUTH_TOKEN.equals(k)) {
+				key = v;
+				LOG.debug("KEY : " + key);
+			} else if (secret == null && OAUTH_TOKEN_SECRET.equals(k)) {
+				secret = v;
+				LOG.debug("SECRET : " + secret);
+			} else {
+				token.setAttribute(k, v);
+			}
+		}
+		if (key != null && secret != null) {
+			token.setKey(key);
+			token.setSecret(secret);
+		}
+	}
+
+	/**
+	 * Adds nonce and timestamp in given parameter map.
+	 * 
+	 * @param params
+	 */
+	public void putNonceAndTimestamp(final Map<String, String> params) {
+		long ts = System.currentTimeMillis();
+		params.put(OAUTH_TIMESTAMP, String.valueOf(ts / 1000));
+		params.put(OAUTH_NONCE, String.valueOf(ts));
+	}
+
+	/**
+	 * Builds the auth url to redirect the user to, based from the given token
+	 * and callback url.
+	 * 
+	 * @param authUrl
+	 * @param token
+	 * @param callbackUrl
+	 *            Callback URL
+	 * @return
+	 * @throws Exception
+	 */
+	public StringBuilder buildAuthUrl(final String authUrl, final Token token,
+			final String callbackUrl) throws Exception {
+		char separator = authUrl.indexOf('?') == -1 ? '?' : '&';
+		return new StringBuilder()
+				.append(authUrl)
+				.append(separator)
+				.append(OAUTH_TOKEN)
+				.append('=')
+				.append(HttpUtil.encodeURIComponent(token.getKey()))
+				.append('&')
+				.append(OAUTH_CALLBACK)
+				.append('=')
+				.append(callbackUrl == null ? OOB : HttpUtil
+						.encodeURIComponent(callbackUrl));
+	}
+
+	/**
+	 * Generates Authorize header
+	 * 
+	 * @param params
+	 *            Parameters to generate header value
+	 * @return Authorize header value
+	 * @throws Exception
+	 */
+	public String getAuthHeaderValue(final Map<String, String> params)
+			throws Exception {
+		StringBuilder headerStr = new StringBuilder();
+		String[] REQUIRED_OAUTH_HEADERS_TO_SIGN = new String[] {
+				OAUTH_CONSUMER_KEY, OAUTH_NONCE, OAUTH_TIMESTAMP,
+				OAUTH_SIGNATURE_METHOD };
+		for (String key : REQUIRED_OAUTH_HEADERS_TO_SIGN) {
+			String value = HttpUtil.encodeURIComponent(params.get(key));
+			headerStr.append(',').append(key).append('=').append('"')
+					.append(value).append('"');
+		}
+		if (params.get(OAUTH_VERSION) != null) {
+			headerStr
+					.append(',')
+					.append(OAUTH_VERSION)
+					.append('=')
+					.append('"')
+					.append(HttpUtil.encodeURIComponent(params
+							.get(OAUTH_VERSION))).append('"');
+		}
+		if (params.get(OAUTH_TOKEN) != null) {
+			headerStr
+					.append(',')
+					.append(OAUTH_TOKEN)
+					.append('=')
+					.append('"')
+					.append(HttpUtil.encodeURIComponent(params.get(OAUTH_TOKEN)))
+					.append('"');
+		}
+		if (params.get(OAUTH_SIGNATURE) != null) {
+			headerStr
+					.append(',')
+					.append(OAUTH_SIGNATURE)
+					.append('=')
+					.append('"')
+					.append(HttpUtil.encodeURIComponent(params
+							.get(OAUTH_SIGNATURE))).append('"');
+		}
+		headerStr.setCharAt(0, ' ');
+		headerStr.insert(0, "OAuth");
+		LOG.debug("Authorize Header : " + headerStr.toString());
+		return headerStr.toString();
 	}
 }
