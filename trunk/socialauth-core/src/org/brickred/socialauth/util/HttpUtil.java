b0c21dc30abe8a56e242d0e76320e522465ad2ea
tarunn@brickred.com <tarunn@brickred.com@3dfb73c5-3865-12c0-3a85-70851a3b1c07>
2011-11-30 15:23:55 +0000

    Code to ignore certificates

--- trunk/socialauth-core/src/org/brickred/socialauth/util/HttpUtil.java
+++ trunk/socialauth-core/src/org/brickred/socialauth/util/HttpUtil.java
@@ -29,10 +29,20 @@ import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.brickred.socialauth.exception.SocialAuthException;
 
@@ -50,6 +60,20 @@ import org.brickred.socialauth.exception.SocialAuthException;
  * 
  */
 public class HttpUtil {
+	static {
+		SSLContext ctx;
+		try {
+			ctx = SSLContext.getInstance("TLS");
+			ctx.init(new KeyManager[0],
+					new TrustManager[] { new DefaultTrustManager() },
+					new SecureRandom());
+			SSLContext.setDefault(ctx);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (KeyManagementException e) {
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * Makes HTTP request using java.net.HTTPURLConnection
@@ -240,4 +264,22 @@ public class HttpUtil {
 		}
 		return buffer.toString();
 	}
+
+	private static class DefaultTrustManager implements X509TrustManager {
+		@Override
+		public void checkClientTrusted(final X509Certificate[] arg0,
+				final String arg1) throws CertificateException {
+		}
+
+		@Override
+		public void checkServerTrusted(final X509Certificate[] arg0,
+				final String arg1) throws CertificateException {
+		}
+
+		@Override
+		public X509Certificate[] getAcceptedIssuers() {
+			return null;
+		}
+	}
+
 }
