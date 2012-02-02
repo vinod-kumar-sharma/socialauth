303a6b93844bca88b498edd2865b99b43c800329
tarunn <tarunn@brickred.com>
2012-02-02 15:56:21 +0000

    Removed getSocialAuthProviderIds() method

--- socialauth-filter/src/de/deltatree/social/web/filter/impl/security/DefaultSASFSocialAuthManager.java
+++ socialauth-filter/src/de/deltatree/social/web/filter/impl/security/DefaultSASFSocialAuthManager.java
@@ -1,9 +1,6 @@
 package de.deltatree.social.web.filter.impl.security;
 
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 import org.brickred.socialauth.SocialAuthConfig;
 import org.brickred.socialauth.SocialAuthManager;
@@ -15,21 +12,8 @@ import de.deltatree.social.web.filter.impl.props.SASFProperties;
 public class DefaultSASFSocialAuthManager implements SASFSocialAuthManager {
 
 	private SocialAuthConfig socialAuthConfig;
-	private final static List<String> socialAuthProviderIds;
-	static {
-		List<String> ids = new ArrayList<String>();
-		ids.add("facebook");
-		ids.add("twitter");
-		ids.add("google");
-		ids.add("yahoo");
-		ids.add("hotmail");
-		ids.add("linkedin");
-		ids.add("foursquare");
-		ids.add("myspace");
-		socialAuthProviderIds = Collections.unmodifiableList(ids);
-	}
 
-	public DefaultSASFSocialAuthManager(SASFProperties props)
+	public DefaultSASFSocialAuthManager(final SASFProperties props)
 			throws SASFSecurityException {
 		SocialAuthConfig config = SocialAuthConfig.getDefault();
 		InputStream in = null;
@@ -59,8 +43,4 @@ public class DefaultSASFSocialAuthManager implements SASFSocialAuthManager {
 		}
 	}
 
-	@Override
-	public List<String> getSocialAuthProviderIds() {
-		return DefaultSASFSocialAuthManager.socialAuthProviderIds;
-	}
 }
