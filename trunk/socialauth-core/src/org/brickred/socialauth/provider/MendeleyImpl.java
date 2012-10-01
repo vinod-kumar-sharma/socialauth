ade767887445203c6c6daca460fdf32d6893677c
tarunn@brickred.com <tarunn@brickred.com@3dfb73c5-3865-12c0-3a85-70851a3b1c07>
2012-10-01 15:36:15 +0000

    Default implementation of uploadImage inherited method. Throws Exception if functionality is not provided.

--- trunk/socialauth-core/src/org/brickred/socialauth/provider/MendeleyImpl.java
+++ trunk/socialauth-core/src/org/brickred/socialauth/provider/MendeleyImpl.java
@@ -25,6 +25,7 @@
 
 package org.brickred.socialauth.provider;
 
+import java.io.InputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -366,5 +367,13 @@ public class MendeleyImpl extends AbstractProvider implements AuthProvider,
 	@Override
 	public String getProviderId() {
 		return config.getId();
+	}
+
+	@Override
+	public Response uploadImage(final String message, final String fileName,
+			final InputStream inputStream) throws Exception {
+		LOG.warn("WARNING: Not implemented for Mendeley");
+		throw new SocialAuthException(
+				"Update Status is not implemented for Mendeley");
 	}
 }
